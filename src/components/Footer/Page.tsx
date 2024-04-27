21b2833197705ea53bf1ff35955dc1d0f6ab1bef
Maciel Zeferino <maciel.zeferino0@gmail.com>
2024-04-27 08:53:18 -0300

    added: taiwind, layout tree parts, header, main, footer, conected in page main.

---
+++ src/components/Footer/Page.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-main-color w-full h-48">
+      <div className="container">
+        <h1>venha nos conhecer</h1>
+      </div>
+    </footer>
+  )
+}
