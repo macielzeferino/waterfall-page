d325b2d86cb3f55f570ee69e9d9e246aeb8804f3
Maciel Zeferino <maciel.zeferino0@gmail.com>
2024-04-30 08:12:05 -0300

    added : basic structure pages

---
+++ src/components/Header/page.tsx
@@ -0,0 +1,7 @@
+export default function Header () {
+  return (
+    <header>
+      <h1>Seja bem vindo a caichoeira do urubu</h1>
+    </header>
+  );
+}
---
+++ src/components/Main/page.tsx
@@ -0,0 +1,14 @@
+export default function Main () {
+  return (
+    <main>
+      <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, minus.</article>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam incidunt aspernatur tempora voluptatibus voluptate sit? Tempora, cupiditate deleniti. Quibusdam beatae, quam consequuntur ad fugiat rerum tempore! Dolorem doloribus odio sapiente!</p>
+      <div className="list">
+        <ul>
+          <li>teste</li>
+          <li>test</li>
+        </ul>
+      </div>
+    </main>
+  );
+}
