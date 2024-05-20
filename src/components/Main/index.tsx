60ce686cd7eaeeb576e933a49f34a13afbe0de09
Maciel Zeferino <maciel.zeferino0@gmail.com>
2024-05-20 08:08:49 -0300

    atualization

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,9 +5,9 @@ export default function Main() {
   return (
     <main className="main">
       <p className="paragraph">   
-       O Parque Ecológico Cachoeira do Urubu é um destino imperdível para quem está passando pelo Piauí. Situado entre as cidades de Esperantina e Batalha, este parque oferece uma beleza natural única, destacada pelas suas impressionantes quedas d'água.
-       A principal atração do parque é a Cachoeira do Urubu, uma das maiores e mais bonitas cachoeiras do estado. Suas águas caem com força sobre as rochas, criando um        espetáculo visual e sonoro que encanta os visitantes. <br /> <br />
-       Além das cachoeiras, o Parque Ecológico Cachoeira do Urubu possui áreas para piquenique e locais de descanso onde os visitantes podem relaxar e apreciar a paisagem. Durante o período das chuvas, de janeiro a junho, o volume de água aumenta, tornando as quedas d'água ainda mais impressionantes. <br />
+       O Parque Ecológico Cachoeira do Urubu é um destino imperdível para quem está passando pelo Piauí. Situado entre as cidades de Esperantina e Batalha, este parque oferece uma beleza natural única, destacada pelas suas impressionantes quedas de água.
+       A principal atração do parque é a Cachoeira do Urubu, uma das maiores e mais bonitas cachoeiras do estado. Suas águas caem com força sobre as rochas, criando um espetáculo visual e sonoro que encanta os visitantes. <br /> <br />
+       Além das cachoeiras, o Parque Ecológico Cachoeira do Urubu possui áreas para piquenique e locais de descanso onde os visitantes podem relaxar e apreciar a paisagem. Durante o período das chuvas, de janeiro a junho, o volume de água aumenta, tornando as quedas de água ainda mais impressionantes. <br />
        Portanto, se você estiver pelo Piauí, não deixe de incluir o Parque Ecológico Cachoeira do Urubu no seu roteiro. Será uma experiência inesquecível, repleta de        contato com a natureza e paisagens deslumbrantes.
       </p>
       <div>
